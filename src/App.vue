dea2b49f245c8bcc7471e6e1f48d0e6c476d45fc
XZY <xuzhenyuan30@gmail.com>
2022-02-15 22:49:35 +0800

    vite

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <h1>网易云音乐</h1>
+  <router-view></router-view>
 </template>
-
-<style lang="less">
+<script setup lang="ts">
+</script>
+<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  * {
+    padding: 0;
+    margin: 0;
   }
 }
 </style>
