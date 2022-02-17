2f222d61863266e650b040e42f85d89cc01cc9c9
xzy <xuzhenyuan30@gmail.com>
2022-02-17 09:39:16 +0800

    antdv

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+import * as Icons from "@ant-design/icons-vue";
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App);
+app.use(router).use(store).use(Antd).mount("#app");
+
+const icons: any = Icons;
+for (const icon in icons) {
+  app.component(icon, icons[icon]);
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
   <h1>网易云音乐</h1>
 </template>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="less">
 h1 {
